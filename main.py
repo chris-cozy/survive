467a77cceaa059eeb93644b4c15d2600f9a91695
chris-cozy <csande9@clemson.edu>
2023-03-29 15:32:31 -0400

    added projectile aiming system

--- main.py
+++ main.py
@@ -34,6 +34,14 @@ x_offset = (SCREEN_WIDTH - PLAYER_WIDTH)/2
 y_offset = (SCREEN_HEIGHT - PLAYER_HEIGHT)/2
 player.set_pos(x_offset, y_offset)
 
+# Projectile Setup #
+proj_start = pygame.math.Vector2(player.rect.center)
+proj_end = proj_start
+proj_len = 50
+PROJ_SPEED = 5
+
+projectiles = []
+
 # Sprite Group Setup #
 spriteList_Room = pygame.sprite.Group()
 spriteList_Room.add(room)
@@ -51,13 +59,38 @@ while playing:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_x:
                 playing = False
+        elif event.type == pygame.MOUSEMOTION:
+            continue
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            proj_distance = mouse - proj_start
+            pos = pygame.math.Vector2(proj_start)
+            proj_speed = proj_distance.normalize() * PROJ_SPEED
+            projectiles.append([pos, proj_speed])
 
+            # Updates #
     player.check_keys(room_bottom, room_top, room_left, room_right)
+    # update aim
+    proj_start = pygame.math.Vector2(player.rect.center)
+    mouse = pygame.mouse.get_pos()
+    proj_end = proj_start + (mouse - proj_start).normalize() * proj_len
+    # updates all projectiles toward their destinations
+    for pos, speed in projectiles:
+        pos += speed
 
-    # Screen Logic #
+    # Screen Logic and Draws #
     screen.fill()
     screen.draw(spriteList_Room)
     screen.draw(spriteList_Ents)
+    # aim
+    pygame.draw.line(screen.surface, (255, 0, 0), proj_start, proj_end)
+    # projectiles
+    for pos, speed in projectiles:
+        pos_x = int(pos.x)
+        pos_y = int(pos.y)
+        pygame.draw.line(screen.surface, (0, 0, 255),
+                         (pos_x, pos_y), (pos_x, pos_y))
+
     screen.flip()
 
     clock.tick(60)
