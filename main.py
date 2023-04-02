8ba76d4dbea27cb96dad446dc164dddf4a09c600
chris-cozy <csande9@clemson.edu>
2023-04-02 11:44:12 -0400

    cleaned the main script

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ ROOM_WIDTH, ROOM_HEIGHT = 800, 600
 PLAYER_WIDTH, PLAYER_HEIGHT = 20, 20
 CAPTION = "survive!"
 
+### HELPER FUNCTIONS ###
+
+
+### MAIN SCRIPT ###
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -29,21 +33,21 @@ room_left = y_offset
 room_right = room_left + ROOM_WIDTH
 room.set_pos(x_offset, y_offset)
 
-# Player Setup #
+# Player Initialization #
 player = Player(PLAYER_WIDTH, PLAYER_HEIGHT)
 x_offset = (SCREEN_WIDTH - PLAYER_WIDTH)/2
 y_offset = (SCREEN_HEIGHT - PLAYER_HEIGHT)/2
 player.set_pos(x_offset, y_offset)
 
-# Debug Player Aim Setup #
+# Player Aim Initialization #
 aim_start = pygame.math.Vector2(player.rect.center)
 aim_end = aim_start
 aim_len = 50
-PROJ_SPEED = 5
 
+# Projectile Initialization #
 projectiles = []
 
-# Sprite Group Setup #
+# Sprite Group Initialization #
 spriteList_Room = pygame.sprite.Group()
 spriteList_Room.add(room)
 
@@ -63,7 +67,6 @@ while playing:
             if event.key == pygame.K_x:
                 playing = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse = pygame.mouse.get_pos()
             projectile = Projectile(aim_start, mouse)
             projectiles.append(projectile)
             spriteList_Projs.add(projectile)
